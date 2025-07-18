e758742fbc3ce61817ccfaf4b3fedce3a7317c6d
Chronix <phoniexpaul88@gmail.com>
2025-07-18 20:42:26 +0530

    Exclude BCR

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,5 @@
 git clone --depth=1 https://github.com/LineageOS-Violet/vendor_xiaomi_violet.git -b lineage-22.2 vendor/xiaomi/violet
 git clone https://github.com/selfmusing/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 git clone https://github.com/zenin1504/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
