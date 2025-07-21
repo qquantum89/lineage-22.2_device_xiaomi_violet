3f4e3c033c4a11a0abc7bed8747193fa422f4fba
Chronix <phoniexpaul88@gmail.com>
2025-07-21 18:49:45 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,4 @@ git clone --depth=1 https://github.com/LineageOS-Violet/vendor_xiaomi_violet.git
 git clone https://github.com/Chronix-AOSP/kernel_xiaomi_violet.git -b main kernel/xiaomi/violet
 git clone --depth=1 https://gitlab.com/CuriousNom/chaste_vendor-xiaomi-firmware-violet.git -b 14.0 vendor/xiaomi-firmware/violet
 git clone --depth=1 https://github.com/selfmusing/meme_cam.git -b 15 vendor/MiuiCamera
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
 git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
