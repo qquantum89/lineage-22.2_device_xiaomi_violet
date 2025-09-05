64a7ef6073dea6c63faeda8a758980eb74be39ec
QQuantum <130094755+qquantum89@users.noreply.github.com>
2025-09-05 13:02:26 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
-# Copyright (C) 2018-2020 The infinityOS Project
+# Copyright (C) 2018-2020 The cherishOS Project
 # Copyright (C) 2020 The PixelExperience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/infinity_violet.mk
+    $(LOCAL_DIR)/cherish_violet.mk
 
 COMMON_LUNCH_CHOICES := \
-    infinity_violet-eng \
-    infinity_violet-userdebug \
-    infinity_violet-user
+    cherish_violet-eng \
+    cherish_violet-userdebug \
+    cherish_violet-user
